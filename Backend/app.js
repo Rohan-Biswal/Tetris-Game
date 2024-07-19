5f58e8582a20084f8ee36f887de690da0da4e669
Rohan-Biswal <rohanbiswal03@gmail.com>
2024-07-19 10:51:30 +0530

    Adding Frontend and Backend Both

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,13 @@ require("dotenv").config();
 const app=express();
 const port= process.env.PORT ||8400;
 const tetrisM=require("./models/tetrisM");
-app.use(cors());
+app.use(cors(
+    {
+        origin:[],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
 app.use(express.json());
 
 app.get("/",(req,res)=>{
